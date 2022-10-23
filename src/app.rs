da1cab654208047536a07d333ba73932a21c105c
Piyawat Lamsam <piyawat3.14@gmail.com>
2022-10-23 13:43:31 +0700

    change ui heading

--- src/app.rs
+++ src/app.rs
@@ -95,7 +95,7 @@ impl eframe::App for TemplateApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
 
-            ui.heading("eframe template");
+            ui.heading("my eframe sample");
             ui.hyperlink("https://github.com/emilk/eframe_template");
             ui.add(egui::github_link_file!(
                 "https://github.com/emilk/eframe_template/blob/master/",
